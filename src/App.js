8b14cace5595293a2266a4715828a9cc5fc96010
pinky-banik <pinky.rani.banik@gmail.com>
2022-10-27 21:51:17 +0600

    project setup

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <img class="w-10 h-10 rounded-full" src={logo} alt="Rounded avatar"/>
+<img class="w-10 h-10 rounded" src={logo} alt="Default avatar"/>
+
+
     </div>
   );
 }
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAWxQcIUoiUh7StVn_Whuf8vvvCcF-d9po",
+  authDomain: "eschool-bd.firebaseapp.com",
+  projectId: "eschool-bd",
+  storageBucket: "eschool-bd.appspot.com",
+  messagingSenderId: "816473128075",
+  appId: "1:816473128075:web:d513a138b07c33d689688e",
+  measurementId: "G-6B39NJFXV2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
