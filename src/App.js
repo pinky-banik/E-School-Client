3fd33e8bd8225d7a450867489e96e2024ea133c5
pinky-banik <pinky.rani.banik@gmail.com>
2022-10-28 17:52:54 +0600

    authprovider,login page and register page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import CourseDetails from './Components/Courses/CourseDetails';
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/courseDetails/:courseId" element={<CourseDetails/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </div>
   );
--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,10 +1,17 @@
+import { getAuth, signInWithPopup } from 'firebase/auth';
 import React from 'react';
 import { createContext } from 'react';
+import app from './../Firebase/firebase.config';
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
-  const user = {displayName :'batash Ali'};
+  const user = null;
+
+  const providerLogin = (provider) =>{
+    return signInWithPopup(auth,provider);
+  }
   const authInfo={user};
 
 
